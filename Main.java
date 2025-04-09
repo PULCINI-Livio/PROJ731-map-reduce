2652f954d2be6f9c04777993b046628783f4d3ed
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-04-09 10:19:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
-
 
 public class Main {
     public static void main(String[] args) {
         String filepath = "data/LesMiserables.txt";
         System.out.println(filepath);
-        List<Map.Entry<String, Integer>> result = MapReduce.mapReduce(filepath);
-        for (Map.Entry<String, Integer> entry : result) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        
+        try {
+            List<Map.Entry<String, Integer>> result = MapReduce.mapReduce(filepath);
+            for (Map.Entry<String, Integer> entry : result) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: The file " + filepath + " was not found.");
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
         }
     }
 }
