5fb8be5a76bd3f93e91d8e67aebf7db828533bc2
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-05-05 08:25:09 +0200

    Update MachineImpl.java

--- MachineImpl.java
+++ MachineImpl.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 
@@ -12,6 +13,11 @@ public class MachineImpl extends UnicastRemoteObject implements Machine {
     public int traiter(int valeur) throws RemoteException {
         System.out.println("Machine " + nom + " traite la valeur " + valeur);
         return valeur * 2;
+    }
+
+    public Map<String, Integer> machineMap(String input) throws RemoteException {
+        Map<String, Integer> result = Mapper.map(input);
+        return result;
     }
 }
 
